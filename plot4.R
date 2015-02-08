894bf7c666abae225792d0ee47a144535013e47c
wkoerbitz <wolfgangkoerbitz@gmail.com>
2015-02-08 22:49:28 +0100

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,17 @@
+setwd("C:/Users/master_Blaster/Documents")
+
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", dec=".", quote="", comment.char="", na.strings="?")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data      <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+data$when <- strptime(paste(data$Date, data$Time, sep=" "), format="%Y-%m-%d %H:%M:%S")
+
+png(file="plot4.png")
+par(mfcol=c(2,2))
+plot(data$when, data$Global_active_power, type="l", ylab="Global Active Power", xlab="")
+plot(data$when, data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+points(data$when, data$Sub_metering_2, type="l", col="red") 
+points(data$when, data$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1, lwd=1, bty="n") 
+plot(data$when, data$Voltage, type="l", ylab="Voltage", xlab="datetime")
+plot(data$when, data$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
+dev.off()
